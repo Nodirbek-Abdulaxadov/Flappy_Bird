e5b34c883c68f895db3a86e600cdb36e706c48c0
Nodirbek-Abdulaxadov <66748319+Nodirbek-Abdulaxadov@users.noreply.github.com>
2021-02-22 18:16:32 +0500

    Add files via upload

---
+++ Flappy Bird/Flappy Bird/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Uchedigan_Qush
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.qush = new System.Windows.Forms.PictureBox();
+            this.tepapip1 = new System.Windows.Forms.PictureBox();
+            this.pastpip1 = new System.Windows.Forms.PictureBox();
+            this.pastpip2 = new System.Windows.Forms.PictureBox();
+            this.tepapip2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.tablo = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.gameover = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.exit_button = new Music_Player_1.ellips_button();
+            this.pause_button = new Music_Player_1.ellips_button();
+            this.start_button = new Music_Player_1.ellips_button();
+            ((System.ComponentModel.ISupportInitialize)(this.qush)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tepapip1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pastpip1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pastpip2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tepapip2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // qush
+            // 
+            this.qush.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.qush.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(216)))), ((int)(((byte)(224)))));
+            this.qush.Image = ((System.Drawing.Image)(resources.GetObject("qush.Image")));
+            this.qush.Location = new System.Drawing.Point(434, 139);
+            this.qush.Name = "qush";
+            this.qush.Size = new System.Drawing.Size(67, 59);
+            this.qush.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.qush.TabIndex = 3;
+            this.qush.TabStop = false;
+            // 
+            // tepapip1
+            // 
+            this.tepapip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(216)))), ((int)(((byte)(224)))));
+            this.tepapip1.Image = ((System.Drawing.Image)(resources.GetObject("tepapip1.Image")));
+            this.tepapip1.Location = new System.Drawing.Point(730, -145);
+            this.tepapip1.Name = "tepapip1";
+            this.tepapip1.Size = new System.Drawing.Size(75, 337);
+            this.tepapip1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tepapip1.TabIndex = 5;
+            this.tepapip1.TabStop = false;
+            // 
+            // pastpip1
+            // 
+            this.pastpip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(216)))), ((int)(((byte)(224)))));
+            this.pastpip1.Image = ((System.Drawing.Image)(resources.GetObject("pastpip1.Image")));
+            this.pastpip1.Location = new System.Drawing.Point(730, 330);
+            this.pastpip1.Name = "pastpip1";
+            this.pastpip1.Size = new System.Drawing.Size(75, 354);
+            this.pastpip1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pastpip1.TabIndex = 6;
+            this.pastpip1.TabStop = false;
+            // 
+            // pastpip2
+            // 
+            this.pastpip2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(216)))), ((int)(((byte)(224)))));
+            this.pastpip2.Image = ((System.Drawing.Image)(resources.GetObject("pastpip2.Image")));
+            this.pastpip2.Location = new System.Drawing.Point(1055, 330);
+            this.pastpip2.Name = "pastpip2";
+            this.pastpip2.Size = new System.Drawing.Size(75, 354);
+            this.pastpip2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pastpip2.TabIndex = 16;
+            this.pastpip2.TabStop = false;
+            // 
+            // tepapip2
+            // 
+            this.tepapip2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(216)))), ((int)(((byte)(224)))));
+            this.tepapip2.Image = ((System.Drawing.Image)(resources.GetObject("tepapip2.Image")));
+            this.tepapip2.Location = new System.Drawing.Point(1055, -145);
+            this.tepapip2.Name = "tepapip2";
+            this.tepapip2.Size = new System.Drawing.Size(75, 337);
+            this.tepapip2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tepapip2.TabIndex = 17;
+            this.tepapip2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(229, 500);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 18;
+            this.pictureBox3.TabStop = false;
+            // 
+            // tablo
+            // 
+            this.tablo.AutoSize = true;
+            this.tablo.BackColor = System.Drawing.Color.Black;
+            this.tablo.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tablo.ForeColor = System.Drawing.Color.White;
+            this.tablo.Location = new System.Drawing.Point(12, 433);
+            this.tablo.Name = "tablo";
+            this.tablo.Size = new System.Drawing.Size(157, 41);
+            this.tablo.TabIndex = 19;
+            this.tablo.Text = "Score: 0";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timeEvent);
+            // 
+            // gameover
+            // 
+            this.gameover.BackColor = System.Drawing.Color.Black;
+            this.gameover.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gameover.ForeColor = System.Drawing.Color.White;
+            this.gameover.Location = new System.Drawing.Point(0, 351);
+            this.gameover.Name = "gameover";
+            this.gameover.Size = new System.Drawing.Size(229, 71);
+            this.gameover.TabIndex = 21;
+            this.gameover.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkRed;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(85, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 19);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Contact to creator";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // exit_button
+            // 
+            this.exit_button.BackColor = System.Drawing.Color.Yellow;
+            this.exit_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exit_button.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exit_button.ForeColor = System.Drawing.Color.DarkRed;
+            this.exit_button.Location = new System.Drawing.Point(19, 215);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(72, 72);
+            this.exit_button.TabIndex = 24;
+            this.exit_button.Text = "EXIT";
+            this.exit_button.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click_1);
+            // 
+            // pause_button
+            // 
+            this.pause_button.BackColor = System.Drawing.Color.Yellow;
+            this.pause_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pause_button.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pause_button.ForeColor = System.Drawing.Color.DarkRed;
+            this.pause_button.Location = new System.Drawing.Point(81, 126);
+            this.pause_button.Name = "pause_button";
+            this.pause_button.Size = new System.Drawing.Size(72, 72);
+            this.pause_button.TabIndex = 23;
+            this.pause_button.Text = "PAUSE";
+            this.pause_button.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.pause_button.Click += new System.EventHandler(this.pause_button_Click);
+            // 
+            // start_button
+            // 
+            this.start_button.BackColor = System.Drawing.Color.Yellow;
+            this.start_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.start_button.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start_button.ForeColor = System.Drawing.Color.DarkRed;
+            this.start_button.Location = new System.Drawing.Point(139, 39);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(72, 72);
+            this.start_button.TabIndex = 22;
+            this.start_button.Text = "START";
+            this.start_button.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(216)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exit_button);
+            this.Controls.Add(this.pause_button);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.gameover);
+            this.Controls.Add(this.tablo);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.tepapip2);
+            this.Controls.Add(this.pastpip2);
+            this.Controls.Add(this.pastpip1);
+            this.Controls.Add(this.tepapip1);
+            this.Controls.Add(this.qush);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pastga);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tepaga);
+            ((System.ComponentModel.ISupportInitialize)(this.qush)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tepapip1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pastpip1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pastpip2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tepapip2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox tepapip1;
+        private System.Windows.Forms.PictureBox pastpip1;
+        public System.Windows.Forms.PictureBox qush;
+        private System.Windows.Forms.PictureBox pastpip2;
+        private System.Windows.Forms.PictureBox tepapip2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label tablo;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label gameover;
+        private Music_Player_1.ellips_button start_button;
+        private Music_Player_1.ellips_button pause_button;
+        private Music_Player_1.ellips_button exit_button;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Flappy Bird/Flappy Bird/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Media;
+
+namespace Uchedigan_Qush
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Stop();
+        }
+
+        int tezlik = -3, gravity = 3, ochko = 0;
+        bool b = true, aa = true, key = true;
+
+        SoundPlayer splayer;
+        private void timeEvent(object sender, EventArgs e)
+        {
+            if (ochko == 5) tezlik = -4;
+            if (ochko == 10) tezlik = -5;
+            if (ochko == 20) tezlik = -6;
+            if (ochko == 30) tezlik = -7;
+            if (ochko == 50) tezlik = -10;
+
+            if (((qush.Left > tepapip1.Left + 50) || (qush.Left > tepapip2.Left + 50)) && (b))
+            {
+                ochko++;
+                tablo.Text = "Score: " + ochko.ToString();
+                b = false;
+                splayer = new SoundPlayer("PlayerCoin.wav");
+                splayer.Play();
+            }
+
+            Random random = new Random();
+            qush.Top += gravity;
+            tepapip1.Left += tezlik;
+            pastpip1.Left += tezlik;
+            tepapip2.Left += tezlik;
+            pastpip2.Left += tezlik;
+
+            if (tepapip1.Left < 162)
+            {
+                tepapip1.Left = tepapip2.Left + 360;
+                pastpip1.Left = pastpip2.Left + 360;
+
+                int a = random.Next(-250, -50);
+                tepapip1.Top = a;
+                pastpip1.Top = a + 500;
+                b = true;
+            }
+            if (tepapip2.Left < 162)
+            {
+                tepapip2.Left = tepapip1.Left + 360;
+                pastpip2.Left = pastpip1.Left + 360;
+                int a = random.Next(-250, -50);
+                tepapip2.Top = a;
+                pastpip2.Top = a + 500;
+                b = true;
+            }
+
+            if (qush.Bounds.IntersectsWith(pastpip1.Bounds) ||
+                qush.Bounds.IntersectsWith(pastpip2.Bounds) ||
+                qush.Bounds.IntersectsWith(tepapip1.Bounds) ||
+                qush.Bounds.IntersectsWith(tepapip2.Bounds) ||
+                qush.Top < 0 || qush.Top > 442)
+            {
+                Tugadi();
+                gameover.Text = "GAME OVER!";
+                start_button.Text = "RESTART";
+                checkpause = false;
+                key = false;
+                splayer = new SoundPlayer("PlayerDie.wav");
+                splayer.Play();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            if (aa)
+            {
+                timer.Start();
+                aa = false;
+                key = true;
+            }
+            else
+            {
+                restart();
+                start_button.Text = "START";
+            }
+        }
+
+        private void exit_button_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        bool checkpause = true;
+        private void pause_button_Click(object sender, EventArgs e)
+        {
+            if ((checkpause)&&(key))
+            {
+                timer.Stop();
+                checkpause = false;
+                pause_button.Text = "PLAY";
+            }
+            else if(key)
+            {
+                timer.Start();
+                checkpause = true;
+                pause_button.Text = "PAUSE";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.instagram.com/nodirbek_09052000/");
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 'A')
+            {
+                ochko += 100;
+            }
+        }
+
+        private void pastga(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                if (ochko < 21) gravity = 3;
+                else gravity = 4;
+            }
+        }
+
+        private void tepaga(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (ochko < 21) gravity = -3;
+                else gravity = -4;
+            }
+        }
+
+        private void Tugadi()
+        {
+            timer.Stop();
+        }
+        public void restart()
+        {
+            qush.Top = 167;
+            tepapip1.Left = 730;
+            pastpip1.Left = 730;
+            tepapip2.Left = tepapip1.Left + 360;
+            pastpip2.Left = pastpip1.Left + 360;
+            ochko = 0;
+            tablo.Text = "Score: " + ochko.ToString();
+            gravity = 3;
+            tezlik = -3;
+            gameover.Text = "";
+            aa = true;
+        }
+    }
+}
